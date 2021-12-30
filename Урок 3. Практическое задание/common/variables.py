60cd0e5f1251ec8d145d5dcab9ab39df543ecf02
Savegorka <savelev.egorka@yandex.ru>
2021-12-30 19:47:24 +0300

    Lesson 3

---
+++ Урок 3. Практическое задание/common/variables.py
@@ -0,0 +1,11 @@
+PORT = 7777
+IP_ADDRESS = '127.0.0.1'
+MAX_PACKAGE_LENGTH = 1024
+RESPONSE = 'response'
+ERROR = 'error'
+TIME = 'time'
+MAX_CONNECTIONS = 5
+ACTION = 'action'
+USER = 'user'
+ACCOUNT_NAME = 'account_name'
+PRESENCE = 'presence'
---
+++ Урок 3. Практическое задание/server.py
@@ -0,0 +1,63 @@
+"""Программа-сервер"""
+
+import socket
+import sys
+import json
+from common.variables import ACTION, ACCOUNT_NAME, RESPONSE, MAX_CONNECTIONS, \
+    PRESENCE, TIME, USER, ERROR, PORT
+from common.utils import get_message, send_message
+
+
+def process_client_message(message):
+    if ACTION in message and message[ACTION] == PRESENCE and TIME in message \
+            and USER in message and message[USER][ACCOUNT_NAME] == 'Guest':
+        return {RESPONSE: 200}
+    return {
+        RESPONSE: 400,
+        ERROR: 'Bad Request'
+    }
+
+
+def main():
+    try:
+        if '-p' in sys.argv:
+            listen_port = int(sys.argv[sys.argv.index('-p') + 1])
+        else:
+            listen_port = PORT
+        if listen_port < 1024 or listen_port > 65535:
+            raise ValueError
+    except IndexError:
+        print('Нет номера порта')
+        sys.exit(1)
+    except ValueError:
+        print(
+            'НЗНД')
+        return ''
+
+    try:
+        if '-a' in sys.argv:
+            listen_address = sys.argv[sys.argv.index('-a') + 1]
+        else:
+            listen_address = ''
+
+    except IndexError:
+        print(
+            'Не указан адрес для сервера')
+        return ''
+
+    transport = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    transport.bind((listen_address, listen_port))
+
+    transport.listen(MAX_CONNECTIONS)
+
+    while True:
+        client, client_address = transport.accept()
+        try:
+            message_from_cient = get_message(client)
+            print(message_from_cient)
+            response = process_client_message(message_from_cient)
+            send_message(client, response)
+            client.close()
+        except (ValueError, json.JSONDecodeError):
+            print('НЗНД')
+            client.close()
